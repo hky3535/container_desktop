21ec9fc464ce603bfd4b1e1e33b40418dfa5ec0f
hky3535 <hky3535@163.com>
2023-07-27 06:24:02 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-apt update
-ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
-&& apt install -y tzdata \
-&& dpkg-reconfigure --frontend noninteractive tzdata
-
-# apt安装所需包
-apt install -y $(cat requirements.apt)
-# 启动vnc-server
-vncserver :5 -geometry 1280x800 -depth 24 -SecurityTypes None
-# 启动noVNC将桌面映射到网页浏览器
+# apt安装所有库
+export DEBIAN_FRONTEND=noninteractive
+echo "tzdata tzdata/Areas select Asia" | debconf-set-selections
+echo "tzdata tzdata/Zones/Asia select Shanghai" | debconf-set-selections
+echo "keyboard-configuration keyboard-configuration/layout select Chinese (simplified) - Chinese (simplified, Pinyin)" | debconf-set-selections
+apt update && apt install -y $(cat requirements.apt)
+# 启动vncserver
+mkdir /root/.vnc && echo "password" | vncpasswd -f > /root/.vnc/passwd && chmod 600 /root/.vnc/passwd
+USER=root vncserver :5 -geometry 1280x800 -depth 24
+# 启动noVNC映射到https网页
 websockify --web=/usr/share/novnc/ --ssl-only --cert=ssl/cert.pem --key=ssl/key.pem 30000 localhost:5905
